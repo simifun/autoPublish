407e69701351da73838312c771ea73f84a25a42b
写诗m <270325369@qq.com>
2018-12-18 23:48:37 +0800

    update

--- app/src/main/java/fun/qsong/autopublish/gif/GifActivity.java
+++ app/src/main/java/fun/qsong/autopublish/gif/GifActivity.java
@@ -1,6 +1,8 @@
 package fun.qsong.autopublish.gif;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.webkit.WebView;
 
 import fun.qsong.autopublish.R;
@@ -8,16 +10,26 @@ import fun.qsong.autopublish.base.BaseActivity;
 
 public class GifActivity extends BaseActivity {
 
-    WebView webView;
+//    WebView webView;
+    GifAdaper gifAdaper;
+    RecyclerView rcvGif;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gif);
-        webView = findViewById(R.id.web_gif);
+//        webView = findViewById(R.id.web_gif);
+        rcvGif = findViewById(R.id.rcv_gif);
+
+        gifAdaper = new GifAdaper();
+        gifAdaper.bindToRecyclerView(rcvGif);
+        rcvGif.setLayoutManager(new LinearLayoutManager(this));
 
 
         GifListBean gifListBean = (GifListBean)getIntent().getSerializableExtra("gifListBeans");
-        webView.loadUrl(gifListBean.data.get(0).getImg_url());
+//        webView.loadUrl(gifListBean.data.get(0).getImg_url());
+        gifAdaper.setNewData(gifListBean.data);
+
     }
 
 
--- app/src/main/java/fun/qsong/autopublish/gif/GifAdaper.java
+++ app/src/main/java/fun/qsong/autopublish/gif/GifAdaper.java
@@ -1,7 +1,9 @@
 package fun.qsong.autopublish.gif;
 
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
 
@@ -20,6 +22,10 @@ public class GifAdaper extends BaseQuickAdapter<GifListBean.Gif, BaseViewHolder>
 
     @Override
     protected void convert(BaseViewHolder helper, GifListBean.Gif item) {
-
+        helper.setText(R.id.tv_gif,item.getName());
+        ImageView ivGif = helper.getView(R.id.iv_gif);
+        Glide.with(mContext)
+                .load(item.getImg_url())
+                .into(ivGif);
     }
 }
